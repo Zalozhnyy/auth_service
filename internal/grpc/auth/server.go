ad2b25f38299c4039c24ec8223707f309f7f4c2e
Zalozhnyy <niczz9797@gmail.com>
2023-11-30 15:22:10 +0300

    implement transport layer

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,14 +5,34 @@ import (
 
 	ssov1 "github.com/Zalozhnyy/auth_proto/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const emptyValue = 0
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPCServer *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{})
+func Register(gRPCServer *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(
@@ -20,7 +40,27 @@ func (s *serverAPI) Login(
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
 
-	panic("implement")
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email required")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "appId required")
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO ..
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.LoginResponse{
+		Token: token,
+	}, nil
 
 }
 
@@ -29,6 +69,21 @@ func (s *serverAPI) Register(
 	req *ssov1.RegisterRequest,
 ) (*ssov1.RegisterResponse, error) {
 
-	panic("implement")
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email required")
+	}
 
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password required")
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO ..
+		return nil, status.Error(codes.InvalidArgument, "internal error")
+	}
+
+	return &ssov1.RegisterResponse{
+		UserId: userID,
+	}, nil
 }
